892135a987b3311021ee7cab6c75f06ff1b9ae2c
Alixes <ixes@mail.ru>
2025-06-28 21:35:34 +0300

    create task1

--- vite-project/src/components/Page1/MainPage.jsx
+++ vite-project/src/components/Page1/MainPage.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import Button from '../Button/Button'
+import React from "react";
+import Button from "../Button/Button";
+import Services from "../Page 3/Services";
 
 const MainPage = () => {
-    return (
-        <>
-            <div>Main Page</div>
-            <Button text="Связаться"></Button>
-        </>
-    )
-}
+  return (
+    <>
+      <div>Main Page</div>
+      <Button text="Связаться"></Button>
+      <Services />
+    </>
+  );
+};
 
-export default MainPage
+export default MainPage;
