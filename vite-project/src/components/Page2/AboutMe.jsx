7e4ee7dd17940f12f27aefaf83450c796003b8ac
liliya-freckle <liliya.nagrimanova@gmail.com>
2025-07-01 15:31:10 +0500

    imgs

--- vite-project/src/components/Page2/AboutMe.jsx
+++ vite-project/src/components/Page2/AboutMe.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 import DiplomaCarusel from './DiplomaCarusel'
 
 // const diplomaImages = [
-//   'src/assets/diploma1.png',
-//   'src/assets/diploma2.png',
-//   'src/assets/diploma3.png',
-//   'src/assets/diploma4.png',
-//   'src/assets/diploma5.png',
-//   'src/assets/diploma6.png',
-//   'src/assets/diploma7.png',
-//   'src/assets/diploma8.png',
-//   'src/assets/diploma9.png',
-//   'src/assets/diploma10.png'
+//   'assets/diploma1.png',
+//   'assets/diploma2.png',
+//   'assets/diploma3.png',
+//   'assets/diploma4.png',
+//   'assets/diploma5.png',
+//   'assets/diploma6.png',
+//   'assets/diploma7.png',
+//   'assets/diploma8.png',
+//   'assets/diploma9.png',
+//   'assets/diploma10.png'
 // ]
 
 
