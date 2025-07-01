1402f43e56ad6032f73c37d06e7f5a668125c5ce
Kretova Olga <kretova0319@gmail.com>
2025-07-01 16:25:18 +0400

    Changed font-family for numbers

--- vite-project/src/components/Page2/AboutMe.jsx
+++ vite-project/src/components/Page2/AboutMe.jsx
@@ -1,30 +1,6 @@
 import React from 'react'
-<<<<<<< Updated upstream
-import DiplomaCarusel from './DiplomaCarusel'
 import Advantages from './Advantages/Advantages'
 import About from './About/About'
-
-// const diplomaImages = [
-//   'assets/diploma1.png',
-//   'assets/diploma2.png',
-//   'assets/diploma3.png',
-//   'assets/diploma4.png',
-//   'assets/diploma5.png',
-//   'assets/diploma6.png',
-//   'assets/diploma7.png',
-//   'assets/diploma8.png',
-//   'assets/diploma9.png',
-//   'assets/diploma10.png'
-// ]
-
-
-
-
-
-=======
-import Advantages from './Advantages/Advantages'
-import About from './About/About'
->>>>>>> Stashed changes
 
 const AboutMe = () => {
   return (
@@ -32,11 +8,7 @@ const AboutMe = () => {
     <About />
     <Advantages />
     </>
-<<<<<<< Updated upstream
- )
-=======
   )
->>>>>>> Stashed changes
 }
 
 export default AboutMe
