b4ab5b6efbd3951713e0a6c089b933804557f6ba
liliya-freckle <liliya.nagrimanova@gmail.com>
2025-07-02 14:00:32 +0500

    commit

--- vite-project/src/components/Page2/About/About.jsx
+++ vite-project/src/components/Page2/About/About.jsx
@@ -8,10 +8,10 @@ export default function About () {
             <h2 className={styles.title}>Обо мне</h2>
             <div className={styles.statistics}>
                 
-      <AboutItem number="3+" description="года я работаю с GetCourse" /> 
-      <AboutItem number="20+" description="онлайн-проектов" subText="Посмотреть проекты" />
-      <AboutItem number="100+" description="собранных лендингов" />
-      <AboutItem number="500+" description="созданных процессов автоматизации" /> 
+            <AboutItem number="3+" description="года я работаю с GetCourse" /> 
+            <AboutItem number="20+" description="онлайн-проектов" subText="Посмотреть проекты" />
+            <AboutItem number="100+" description="собранных лендингов" />
+            <AboutItem number="500+" description="созданных процессов автоматизации" /> 
     </div>
             
         </div>
