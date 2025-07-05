5767f6edf10845a529294dbcb0720b3ce9c0f13d
Kretova Olga <kretova0319@gmail.com>
2025-07-05 22:58:35 +0400

    Finished JobStages page

--- src/components/Page5/JobStages.jsx
+++ src/components/Page5/JobStages.jsx
@@ -7,12 +7,12 @@ const JobStages = () => {
     <div id="jobstages">
         <h2 className={styles.title}>Этапы работы</h2>
         <div className={styles.stages}> 
-                <JobStagesItem number="1 этап" description="Вы оставляете заявку" /> 
-                <JobStagesItem number="2 этап" description="Мы созваниваемся или связываемся другим удобным для Вас способом для консультации" />
-                <JobStagesItem number="3 этап" description="Я составляю для Вас поэтапный план действий и расчет стоимости услуг" />
-                <JobStagesItem number="4 этап" description="Вы утверждаете расчет и вносите предоплату 50% от полной стоимости" /> 
-                <JobStagesItem number="5 этап" description="После завершения работы вы вносите остаток оплаты, и я передаю Вам все доступы и материалы" /> 
-                <JobStagesItem number="6 этап" description="Я приступаю к работе в течение 1-5 дней после предоставления Вами всех необходимых материалов" /> 
+                <JobStagesItem number="1" word="этап" description="Вы оставляете заявку" /> 
+                <JobStagesItem number="2" word="этап" description="Мы созваниваемся или связываемся другим удобным для Вас способом для консультации" />
+                <JobStagesItem number="3" word="этап" description="Я составляю для Вас поэтапный план действий и расчет стоимости услуг" />
+                <JobStagesItem number="4" word="этап" description="Вы утверждаете расчет и вносите предоплату 50% от полной стоимости" /> 
+                <JobStagesItem number="5" word="этап" description="После завершения работы вы вносите остаток оплаты, и я передаю Вам все доступы и материалы" /> 
+                <JobStagesItem number="6" word="этап" description="Я приступаю к работе в течение 1-5 дней после предоставления Вами всех необходимых материалов" /> 
             </div>
                     
 
--- src/components/Page5/JobStagesItem.jsx
+++ src/components/Page5/JobStagesItem.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import styles from './jobstages.module.scss';
 
-export default function JobStagesItem({ number, description }) {
+export default function JobStagesItem({ number, word, description }) {
   return (
     <div className={styles.stageItem}>
-      <div className={styles.stageNumber}>{number}</div>
+      <div className={styles.stageNumberWrapper}>
+        <span className={styles.stageNumber}>{number}</span>
+        <span className={styles.stageWord}>{word}</span>
+      </div>
       <div className={styles.stageDescription}>{description}</div>
     </div>
   );
 }
+
 
