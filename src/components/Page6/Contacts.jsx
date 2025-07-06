12e402d0f9492a9e6e4182ac047068eab6ccc555
Kretova Olga <kretova0319@gmail.com>
2025-07-06 12:09:43 +0400

    Added Contacts page

--- src/components/Page6/Contacts.jsx
+++ src/components/Page6/Contacts.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
 import styles from './contacts.module.scss';
+import iconWA from "/icons/icon-WA.png"
+import iconTG from "/icons/icon-TG.png"
+import iconVK from "/icons/icon-VK.png"
 
 const Contacts = () => {
   return (
     <div id="contacts">
         <h2 className={styles.title}>Контакты</h2>
+        <div className={styles.contactsContainer}>
+          <p>Вы можете связаться со мной любым удобным для Вас способом</p>
+          <p>+7 913 157 89-33</p>
+          <p>kovaleva070215@gmail.com</p>
+        <div className={styles.iconsContainer}>
+          <a href="https://wa.me/79131578933" target="_blank" rel="noopener noreferrer">
+                <img src={iconWA} alt="WhatsApp" className={styles.icon} />
+            </a>
+            <a href="https://t.me/miss_Kovaleva_Tatiana" target="_blank" rel="noopener noreferrer">
+                <img src={iconTG} alt="Telegram" className={styles.icon} />
+            </a>
+            
+            <a href="https://vk.com/miss_kovaleva_tatiana" target="_blank" rel="noopener noreferrer">
+                <img src={iconVK} alt="VK" />
+            </a>
+        </div>
+        </div>
+        
     </div>
 
   )
