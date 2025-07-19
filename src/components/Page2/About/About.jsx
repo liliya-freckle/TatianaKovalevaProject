adfd357e457d5685ea0f82e455984c129ddc83e6
Kretova Olga <kretova0319@gmail.com>
2025-07-19 23:34:09 +0400

    settings

--- src/components/Page2/About/About.jsx
+++ src/components/Page2/About/About.jsx
@@ -1,30 +1,41 @@
-import React from 'react';
+import React from "react";
 import { useState } from "react";
-import styles from './about.module.scss';
-import AboutItem from './AboutItem';
-import ModalContent from './ModalContent';
-import Modal from './Modal';
+import styles from "./about.module.scss";
+import AboutItem from "./AboutItem";
+import ModalContent from "./ModalContent";
+import Modal from "./Modal";
 
-export default function About () {
+export default function About() {
+  const [isOpen, setIsOpen] = useState(false);
 
-    const [isOpen, setIsOpen] = useState(false);
-
-    return (
-        <section className={styles.aboutSection}>
-            <h2 className={styles.title}>Обо мне</h2>
-            <div className={styles.statistics}> 
-                <AboutItem number="3+" description="года я работаю с GetCourse" className={styles.firstItem}/> 
-                <AboutItem number="20+" description="онлайн-проектов" subText="Посмотреть проекты" setIsOpen={setIsOpen}/>
-                <AboutItem number="100+" description="собранных лендингов" />
-                <AboutItem number="500+" description="созданных процессов автоматизации" /> 
-            </div>
-            <div>
-                {isOpen && (
-                    <Modal setIsOpen={setIsOpen}>
-                        <ModalContent setIsOpen ={setIsOpen} />
-                    </Modal>
-                )}
-            </div>
-        </section>
-    );
-};
+  return (
+    <section className={styles.aboutSection}>
+      <h2 className={styles.title}>Обо мне</h2>
+      <div className={styles.statistics}>
+        <AboutItem
+          number="3+"
+          description="года я работаю с GetCourse"
+          className={styles.firstItem}
+        />
+        <AboutItem
+          number="20+"
+          description="онлайн-проектов"
+          subText="Посмотреть проекты"
+          setIsOpen={setIsOpen}
+        />
+        <AboutItem number="100+" description="собранных лендингов" />
+        <AboutItem
+          number="500+"
+          description="созданных процессов автоматизации"
+        />
+      </div>
+      <div>
+        {isOpen && (
+          <Modal setIsOpen={setIsOpen}>
+            <ModalContent setIsOpen={setIsOpen} />
+          </Modal>
+        )}
+      </div>
+    </section>
+  );
+}
