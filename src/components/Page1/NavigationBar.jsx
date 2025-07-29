3c6905e992902ed24cb4133e97467da519f3f98a
rkvrpl <rkvrpl@icloud.com>
2025-07-29 18:58:22 +0300

    fixed mobile nav

--- src/components/Page1/NavigationBar.jsx
+++ src/components/Page1/NavigationBar.jsx
@@ -1,54 +1,94 @@
 import './NavigationBar.scss'
-import React, { useState } from 'react';
-
+import React, { useState } from 'react'
 
 const NavigationBar = () => {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-  
-  const scrollToWithOffset = (id) => {
-    const element = document.getElementById(id);
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  const scrollToWithOffset = id => {
+    const element = document.getElementById(id)
     if (element) {
-      const offset = window.innerWidth <= 1000 ? 50 : 90;
-      const bodyRect = document.body.getBoundingClientRect().top;
-      const elementRect = element.getBoundingClientRect().top;
-      const elementPosition = elementRect - bodyRect;
-      const offsetPosition = elementPosition - offset;
+      const offset = window.innerWidth <= 1000 ? 50 : 90
+      const bodyRect = document.body.getBoundingClientRect().top
+      const elementRect = element.getBoundingClientRect().top
+      const elementPosition = elementRect - bodyRect
+      const offsetPosition = elementPosition - offset
 
       window.scrollTo({
         top: offsetPosition,
-        behavior: 'smooth'
-      });
-      setIsMenuOpen(false);
+        behavior: 'smooth',
+      })
+      setIsMenuOpen(false)
     }
-  };
-  const handleNavClick = () => {
-        setIsMenuOpen(false);
-    };
+  }
+  const scrollToWithOffsetMobile = id => {
+    const element = document.getElementById(id)
+    if (element) {
+      const offset = 40
+      const bodyRect = document.body.getBoundingClientRect().top
+      const elementRect = element.getBoundingClientRect().top
+      const elementPosition = elementRect - bodyRect
+      const offsetPosition = elementPosition - offset
+
+      window.scrollTo({
+        top: offsetPosition,
+        behavior: 'smooth',
+      })
+      setIsMenuOpen(false)
+    }
+  }
   return (
     <>
-    <div className='burger_container'>
-    <div 
-        className={`burger-icon ${isMenuOpen ? 'open' : ''}`} 
-        onClick={() => setIsMenuOpen(!isMenuOpen)}
-      >
-        <span></span>
-        <span></span>
-        <span></span>
+      <div className='burger_container'>
+        <div
+          className={`burger-icon ${isMenuOpen ? 'open' : ''}`}
+          onClick={() => setIsMenuOpen(!isMenuOpen)}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
       </div>
-    </div>
-    <nav className={`nav-links ${isMenuOpen ? 'active' : ''}`}>
-      <a href="#aboutme" onClick={handleNavClick}>Обо мне</a>
-      <a href="#services"onClick={(e) => {
-              e.preventDefault();
-              scrollToWithOffset('services');
-            }}>Услуги</a>
-      <a href="#cases" onClick={(e) => {
-              e.preventDefault();
-              scrollToWithOffset('cases');
-            }}>Кейсы</a>
-      <a href="#jobstages" onClick={handleNavClick}>Этапы работы</a>
-      <a href="#contacts" onClick={handleNavClick}>Контакты</a>
-    </nav>
+      <nav className={`nav-links ${isMenuOpen ? 'active' : ''}`}>
+        <a
+          href='#aboutme'
+          onClick={e => {
+            e.preventDefault()
+            if (window.innerWidth <= 1000)scrollToWithOffsetMobile('aboutme')
+          }}>
+          Обо мне
+        </a>
+        <a
+          href='#services'
+          onClick={e => {
+            e.preventDefault()
+            scrollToWithOffset('services')
+          }}>
+          Услуги
+        </a>
+        <a
+          href='#cases'
+          onClick={e => {
+            e.preventDefault()
+            scrollToWithOffset('cases')
+          }}>
+          Кейсы
+        </a>
+        <a
+          href='#jobstages'
+          onClick={e => {
+            e.preventDefault()
+            if (window.innerWidth <= 1000)scrollToWithOffsetMobile('jobstages')
+          }}>
+          Этапы работы
+        </a>
+        <a
+          href='#contacts'
+          onClick={e => {
+            e.preventDefault()
+            if (window.innerWidth <= 1000)scrollToWithOffsetMobile('contacts')
+          }}>
+          Контакты
+        </a>
+      </nav>
     </>
   )
 }
